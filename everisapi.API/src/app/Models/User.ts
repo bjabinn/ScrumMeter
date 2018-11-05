ca0e45d377f27b4e6d3c5ef2ba7cc67ead1bb796
Carlos Cayeiro Mancera <carloscayeiro@gmail.com>
2018-11-05 11:25:46 +0100

    User with role fix

--- everisapi.API/src/app/Models/User.ts
+++ everisapi.API/src/app/Models/User.ts
@@ -1,15 +1,6 @@
 export class User{
-
-	public nombreUsuario: string;
-	public passwordUsuario: string;
-	public role: any;
-
 	constructor(
 		public nombre: string, 
         public password: string
-		){
-			this.nombreUsuario = nombre;
-			this.passwordUsuario = password
-		
-		}
+		){}
 }
--- everisapi.API/src/app/back-office/components/user-management/user-management.component.ts
+++ everisapi.API/src/app/back-office/components/user-management/user-management.component.ts
@@ -1,66 +1,73 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../../services/UserService';
-import { User } from 'app/Models/User';
+import { ProyectoService } from 'app/services/ProyectoService';
+import { Router } from "@angular/router";
+import { UserWithRole } from 'app/Models/UserWithRole';
+import { AppComponent } from 'app/app.component';
+
+
+
+
 
 @Component({
   selector: 'app-user-management',
   templateUrl: './user-management.component.html',
   styleUrls: ['./user-management.component.scss'],
-  providers: [UserService]
+  providers: [UserService, ProyectoService]
 })
+
+
+
 export class UserManagementComponent implements OnInit {
 
-  // elements: any = [];
-  // headElements = ['ID', 'First', 'Last', 'Handle'];
-
-  // searchText: string = '';
-  // previous: string;
+  public ErrorMessage: string = null;
+  public ListaDeUsuarios: UserWithRole[] = [];
+  name = 'Angular 5';
+  query:string = '';
   
-  public ListaDeUsuarios: Array<User> = [];
-  constructor(private _UserService: UserService) { }
-
-  //@HostListener('input') 
-  // oninput() {
-  //   this.searchItems();
-  // }
+  
+  constructor(
+    private _UserService: UserService, 
+    private _proyectoService: ProyectoService,
+    private _router: Router,
+    private _appComponent: AppComponent
+    ){ }
 
   ngOnInit() {
-    //for (let i = 1; i <= 10; i++) {
-      //this.elements.push({ id: i.toString(), first: 'Wpis ' + i, last: 'Last ' + i, handle: 'Handle ' + i });
-      this.searchItems();
+    //Empezamos cargando el usuario en el componente mientras verificamos si esta logueado
+    //En casao de no estar logeado nos enviara devuelta al login
+    if (!this._proyectoService.verificarUsuario()) {
+      this._router.navigate(['/login']);
+    }
+    this.getAllUsers();
+
+    console.log(this.ListaDeUsuarios);
     }
 
-    //this._UserService.setDataSource(this.elements);
-    //this.elements = this._UserService.getUsers();
-    //this.previous = this._UserService.getUsers();
-  
 
-  public searchItems() {
-     //this.usuarios = this._UserService.getUsers();
+  public getAllUsers(){
 
      this._UserService.getUsers().subscribe(
-      res => {
-        console.log(res);
-        this.ListaDeUsuarios = res;
+      res => {this.ListaDeUsuarios = res; 
         console.log(this.ListaDeUsuarios);
-      },
+      }
+      ,
       error => {
         //Si el servidor tiene algún tipo de problema mostraremos este error
-        console.log('explotido');
+        if (error == 404) {
+          this.ErrorMessage = "Error: " + error + " El usuario o proyecto autenticado no existe.";
+        } else if (error == 500) {
+          this.ErrorMessage = "Error: " + error + " Ocurrio un error en el servidor, contacte con el servicio técnico.";
+        } else if (error == 401) {
+          this.ErrorMessage = "Error: " + error + " El usuario es incorrecto o no tiene permisos, intente introducir su usuario nuevamente.";
+        } else {
+          this.ErrorMessage = "Error: " + error + " Ocurrio un error en el servidor, contacte con el servicio técnico.";
+        }
       });
 
      
   }
 
-    // if (!this.searchText) {
-    //   this._UserService.setDataSource(this.previous);
-    //   this.elements = this._UserService.getUsers();
-    // }
-
-    // if (this.searchText) {
-    //   this.elements = this._UserService.searchLocalDataBy(this.searchText);
-    //   this._UserService.setDataSource(prev);
-    // }
 
   
 
