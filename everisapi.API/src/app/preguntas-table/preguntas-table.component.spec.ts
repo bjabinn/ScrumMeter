34582622f318d6625915fe29625a1285af6af84a
@fvellari <fvellari@everis.com>
2018-11-15 18:52:25 +0000

    fix tabla preguntas component.spec

--- everisapi.API/src/app/preguntas-table/preguntas-table.component.spec.ts
+++ everisapi.API/src/app/preguntas-table/preguntas-table.component.spec.ts
@@ -1,19 +1,19 @@
 
 import { fakeAsync, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SortedTableComponent } from './sorted-table.component';
+import { PreguntasTableComponent } from './preguntas-table.component';
 
-describe('SortedTableComponent', () => {
-  let component: SortedTableComponent;
-  let fixture: ComponentFixture<SortedTableComponent>;
+describe('PreguntasTableComponent', () => {
+  let component: PreguntasTableComponent;
+  let fixture: ComponentFixture<PreguntasTableComponent>;
 
   beforeEach(fakeAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ SortedTableComponent ]
+      declarations: [ PreguntasTableComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(SortedTableComponent);
+    fixture = TestBed.createComponent(PreguntasTableComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
