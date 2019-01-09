2068f0218bf6242e18cd43f00e8453fd8016a36a
JavierH <javihr95@gmail.com>
2019-01-09 10:40:15 +0100

    Peso Preguntas int to float

---
+++ everisapi.API/Migrations/20190109093506_PesoIntToFloat.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace everisapi.API.Migrations
+{
+    public partial class PesoIntToFloat : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<float>(
+                name: "Peso",
+                table: "Preguntas",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldMaxLength: 50);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Peso",
+                table: "Preguntas",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(float));
+        }
+    }
+}
