555f2ab8996677b031bc21201cafde3f6ae8bd4c
@fvellari <fvellari@everis.com>
2019-03-26 17:45:50 +0000

    docker-compose

--- docker-compileRunAgileMeter.bat
+++ docker-compileRunAgileMeter.bat
@@ -1,2 +1,2 @@
 docker build -t "agilemeter" ./everisapi.API
-docker run --name agilemeter --link autodumperagile:database -p 60406:60406 agilemeter
+docker run --name agilemeter --link agiledatabase:database -p 60406:60406 agilemeter
