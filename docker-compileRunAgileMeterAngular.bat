2fe48750e519bb4ac9003ef2f8ac88cef9a15713
@fvellari <fvellari@everis.com>
2019-03-26 01:46:37 +0000

    npm dependencies fix for docker

--- docker-compileRunAgileMeterAngular.bat
+++ docker-compileRunAgileMeterAngular.bat
@@ -1,2 +1,3 @@
-docker docker build -f everisapi.API/Dockerfile_Angular -t "agilemeter_angular" .
-docker run --name agilemeter_angular -p 4200:4200 agilemeter_angular
+#docker build -f everisapi.API/Dockerfile_Angular -t "agilemeter_angular" .
+docker build -t "agilemeter_front" ./front
+docker run --name agilemeter_front -p 4200:4200 agilemeter_front
